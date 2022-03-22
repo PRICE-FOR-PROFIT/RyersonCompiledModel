439130a8daf0e7e0fad3aba786d336e562fb8990
Bob Maurer <bmaurer@insight2profit.com>
2022-03-22 10:55:09 -0400

    Initial API project created

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask
+from flasgger import Swagger
+from api.route.home import home_api
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+    app.config['SWAGGER'] = {
+        'title': 'Ryerson Compiled Model',
+    }
+    swagger = Swagger(app)
+
+    # Initialize Config
+    app.config.from_pyfile('config.py')
+    app.register_blueprint(home_api, url_prefix='/api')
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+
+    app = create_app()
+
+    app.run(host='0.0.0.0', port=port)
