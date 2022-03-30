f965df263dbe451bee83c4c3853fd236737e7f55
Bob Maurer <bmaurer@insight2profit.com>
2022-03-30 09:42:26 -0400

    Inputs converted to JSON correctly.

--- api/resources/calculation.py
+++ api/resources/calculation.py
@@ -1,3 +1,4 @@
+import json
 from uuid import uuid4
 from flask import request
 from flask_restful import Resource
@@ -8,12 +9,59 @@ from api.schema.errorwrapperwithmeta import ErrorWrapperWithMetaSchema
 from api.service.authenticationhelper import AuthenticationHelper
 
 
+def lower_keys(data):
+    if isinstance(data, list):
+        return [lower_keys(v) for v in data]
+    elif isinstance(data, dict):
+        return {k.lower(): lower_keys(v) for (k, v) in data.items()}
+    else:
+        return data
+
+
 def generate_error_wrapper_with_meta(code: str, description: str, calculation_id: str) -> dict:
     metadata = {"calculationIid": calculation_id}
 
     error = {"responseCode": code, "description": description}
 
     return {"error": error, "metadata": metadata}
+
+
+def transform_input_list_object_array(data: [dict]) -> [str]:
+    transformed_data = {}
+
+    for line in data:
+        property_name = line["name"]
+        property_value = line["value"]
+
+        if isinstance(property_value, list):
+            transformed_data[property_name] = [transform_input_list_object_array(property_value[0])]
+        else:
+            transformed_data[property_name] = property_value
+
+    return transformed_data
+
+
+def extract_properties_to_include_in_response(data: dict) -> [str]:
+    try:
+        properties = []
+
+        for line in data:
+            key = line["name"]
+            return_in_output = False if line["returninoutput"] is None else line["returninoutput"]
+
+            if return_in_output and not (key in properties):
+                properties.append(key)
+
+            if key == "":
+                print("")
+
+            if line["value"] and isinstance(line["value"], dict):
+                properties.append(extract_properties_to_include_in_response(line["value"]))
+
+        return properties
+    except Exception as ex:
+        print(ex)
+        return ""
 
 
 class CalculationApi(Resource):
@@ -86,10 +134,25 @@ class CalculationApi(Resource):
 
         metadata = {"calculationid": calculation_id}
 
-        payload = request.get_json()
+        calculation_inputs = lower_keys(request.get_json())
 
-        if "InputParameters" in payload:
-            pass
+        if calculation_inputs["inputparameters"] is not None:
+            try:
+                input_data = calculation_inputs["inputparameters"]
+
+                properties = extract_properties_to_include_in_response(input_data)
+                converted_inputs = [transform_input_list_object_array(input_data)]
+                
+                calculation_inputs["modelinputs"] = converted_inputs
+                calculation_inputs["includeinresponse"] = properties
+            except Exception as ex:
+                error_info = f"Error evaluating model: {model_id}"
+
+                ex = Exception(error_info)
+
+                output = generate_error_wrapper_with_meta(HTTPStatus.NOT_FOUND, error_info, calculation_id)
+
+                return ErrorWrapperWithMetaSchema().dump(output), HTTPStatus.BAD_REQUEST
 
         result = [{"name": "John", "value": 30}]
 
